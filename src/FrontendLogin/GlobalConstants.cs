423c797cd9f14c658ca9d1ba4661e05e72b09909
js <js@7orcas.com>
2026-06-25 14:43:19 +1200

    Checkpoint

--- src/FrontendLogin/GlobalConstants.cs
+++ src/FrontendLogin/GlobalConstants.cs
@@ -2,9 +2,10 @@
 {
     public class GlobalConstants : Common.GlobalConstants
     {
-        public const string HTTP_Client    = "BackendApi";
-        public const string URL_login      = "api/Login/login";
-        public const string URL_mfa_verify = "api/Mfa/verifyMfa";
-        public const string URL_mfa_setup  = "api/Mfa/setupMfa";
+        public const string HTTP_Client      = "BackendApi";
+        public const string URL_login        = "api/Login/login";
+        public const string URL_reset_action = "api/Login/resetaction";
+        public const string URL_mfa_verify   = "api/Mfa/verifyMfa";
+        public const string URL_mfa_setup    = "api/Mfa/setupMfa";
     }
 }
