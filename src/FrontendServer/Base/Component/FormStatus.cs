f542afbfe8ccfea16c4200aaeb3450ff684cc765
js <js@7orcas.com>
2026-08-17 18:24:49 +1200

    Checkpoint added FormStatus

---
+++ src/FrontendServer/Base/Component/FormStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace FrontendServer.Base.Component
+{
+    public class FormStatus
+    {
+        public LabelService LS { get; set; }
+
+
+        public bool IsEditMode { get; set; } = false;
+        
+        public FormStatus ToggleEdit()
+        {
+            IsEditMode = !IsEditMode;
+            return this;
+        }
+
+        public string GetEditModeLabel()
+        {
+            if (LS == null) return IsEditMode ? "ViewX" : "EditX";
+            return IsEditMode ? LS.GetLabel("ViewM") : LS.GetLabel("EditM");
+        }
+    }
+}
